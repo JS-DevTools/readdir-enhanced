417a1f5558e5077d2878c116323b46eef8ca528d
James Messinger <james.messinger@gmail.com>
2016-08-24 06:38:49 -0500

    Use `stream.Readable` in sync mode, rather than creating a synchronous Stream facade

---
+++ test/specs/stream.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * TODO: Add tests for the streaming interface
+ *
+ *  - test piping to other streams
+ *  - test pausing/resuming the stream
+ *  - test subscribing to the "file" event without subscribing to the "data" event
+ *
+ */
