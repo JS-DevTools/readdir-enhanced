bacf80ddcd939fb486d532553d9e0b4b8160772c
Denis Malinochkin <mrmlnc@yandex-team.ru>
2017-11-19 18:02:21 +0300

    feat(reader): Ability to use depth in the custom deep function

--- test/specs/deep.js
+++ test/specs/deep.js
@@ -174,6 +174,32 @@ describe('options.deep', function () {
       }
     },
     {
+      it: 'should recurse based on a custom function with depth property',
+      args: ['test/dir', {
+        deep: function (stats) {
+          return stats.depth !== 1;
+        },
+      }],
+      assert: function (error, data) {
+        expect(error).to.be.null;
+        expect(data).to.have.same.members(this.omitSubdir(dir.deep.data));
+      },
+      streamAssert: function (errors, data, files, dirs, symlinks) {
+        expect(errors.length).to.equal(0);
+        expect(data).to.have.same.members(this.omitSubdir(dir.deep.data));
+        expect(files).to.have.same.members(this.omitSubdir(dir.deep.files));
+        expect(dirs).to.have.same.members(this.omitSubdir(dir.deep.dirs));
+        expect(symlinks).to.have.same.members(this.omitSubdir(dir.deep.symlinks));
+      },
+
+      // Omits the contents of the "subdir" directory
+      omitSubdir: function (paths) {
+        return paths.filter(function (p) {
+          return p.split(path.sep).length <= 2;
+        });
+      }
+    },
+    {
       it: 'should return all deep contents if custom deep function always returns true',
       args: ['test/dir', {
         deep: function () {
