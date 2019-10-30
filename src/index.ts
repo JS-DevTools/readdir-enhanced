1e3032f60ea472cb3c043f94b15c4bd4007f0c0e
James Messinger <james.messinger@gmail.com>
2019-10-30 07:46:51 -0500

    Defined the top-level `Readdir` interface, which contains all of the other interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
 // tslint:disable: no-default-export no-unsafe-any
-import { readdirAsync } from "./async";
+import { async, readdirAsync, ReaddirAsync } from "./async";
+import { ReaddirStream, stream } from "./stream";
+import { ReaddirSync, sync } from "./sync";
+
+/**
+ * A backward-compatible drop-in replacement for Node's built-in `fs.readdir()` function that adds
+ * support for additional features like filtering, recursion, absolute paths, and streaming.
+ */
+export interface Readdir extends ReaddirAsync {
+  sync: ReaddirSync;
+  async: ReaddirAsync;
+  stream: ReaddirStream;
+}
+
+// Export type definitions
+export * from "./types-public";
 
 // Export the sync, async, and streaming interfaces
 export { async, ReaddirAsync, readdirAsync, readdirAsyncStat } from "./async";
 export { ReaddirStream, readdirStream, readdirStreamStat, stream } from "./stream";
 export { ReaddirSync, readdirSync, readdirSyncStat, sync } from "./sync";
 
-// Export type definitions
-export * from "./types-public";
+const readdir = readdirAsync as Readdir;
+readdir.sync = sync;
+readdir.async = async;
+readdir.stream = stream;
 
-// Export a drop-in replacement for Node's fs.readdir() function
-export { readdirAsync as readdir };
-export default readdirAsync;
+export { readdir };
+export default readdir;
 
 // CommonJS default export hack
 if (typeof module === "object" && typeof module.exports === "object") {
