cf635bf2a0fd1b3386cf4a33b8d22946018e6019
James Messinger <james.messinger@gmail.com>
2019-11-09 08:46:12 -0600

    Fixed the `isStats()` utility function to work on all platforms and Node versions

--- test/utils/is-stats.js
+++ test/utils/is-stats.js
@@ -14,14 +14,14 @@ function isStats (stats) {
   expect(stats.atimeMs).to.be.a("number").above(0);
   expect(stats.birthtime).to.be.an.instanceOf(Date);
   expect(stats.birthtimeMs).to.be.a("number").above(0);
-  expect(stats.blksize).to.be.a("number").above(0);
+  expect(stats.blksize).to.be.a("number").at.least(0);
   expect(stats.blocks).to.be.a("number").at.least(0);
   expect(stats.ctime).to.be.an.instanceOf(Date);
   expect(stats.ctimeMs).to.be.a("number").above(0);
   expect(stats.depth).to.be.a("number").at.least(0);
-  expect(stats.dev).to.be.a("number").above(0);
+  expect(stats.dev).to.be.a("number").at.least(0);
   expect(stats.gid).to.be.a("number").at.least(0);
-  expect(stats.ino).to.be.a("number").above(0);
+  expect(stats.ino).to.be.a("number").at.least(0);
   expect(stats.mode).to.be.a("number").above(0);
   expect(stats.mtime).to.be.an.instanceOf(Date);
   expect(stats.mtimeMs).to.be.a("number").above(0);
